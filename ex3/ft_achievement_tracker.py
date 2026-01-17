516fcd80568c220f15e8a8ff5aa55fdd7570da35
Hicham Ouzddou <hiouzddo@c4r9p9.1337.ma>
2026-01-17 22:22:02 +0100

    push python 03

--- ex3/ft_achievement_tracker.py
+++ ex3/ft_achievement_tracker.py
@@ -1,11 +1,35 @@
+#!/usr/bin/env python3
 def achievement():
-    Player1 = ["first_kill", "level_10", "treasure_hunter", "speed_demon"]
-    Player2 = ["first_kill", "level_10", "boss_slayer", "collector"]
-    Player3 = ["level_10", "treasure_hunter", "boss_slayer", "\
-speed_demon", "perfectionist"]
-    print(f"Player alice achievements: {set(Player1)}")
-    print(f"Player bob achievements: {set(Player2)}")
-    print(f"Player charlie achievements: {set(Player3)}")
+    Player_alice = {"first_kill", "level_10", "treasure_hunter", "speed_demon"}
+    Player_bob = {"first_kill", "level_10", "boss_slayer", "collector"}
+    Player_charlie = {"level_10", "treasure_hunter", "boss_slayer", "\
+speed_demon", "perfectionist"}
+
+    print("=== Achievement Tracker System ===")
+    print(f"Player alice achievements: {Player_alice}")
+    print(f"Player bob achievements: {Player_bob}")
+    print(f"Player charlie achievements: {Player_charlie}")
+
+    print("\n=== Achievement Analytics ===")
+
+    print("All unique achievements: ",
+          (Player_alice.union(Player_bob, Player_charlie)))
+    print("Total unique achievements: ",
+          len(Player_alice.union(Player_bob, Player_charlie)))
+
+    print("\nCommon to all players: ",
+          Player_alice.intersection(Player_bob, Player_charlie))
+
+    alice_rare = Player_alice.difference(Player_bob, Player_charlie)
+    bob_rare = Player_bob.difference(Player_alice, Player_charlie)
+    charlie_rare = Player_charlie.difference(Player_alice, Player_bob)
+
+    print("Rare achievements (1 player): ",
+          alice_rare.union(bob_rare, charlie_rare))
+
+    print("\nAlice vs Bob common:", Player_alice.intersection(Player_bob))
+    print("Alice unique:", Player_alice.difference(Player_bob))
+    print("Bob unique:", Player_bob.difference(Player_alice))
 
 
 if __name__ == "__main__":
